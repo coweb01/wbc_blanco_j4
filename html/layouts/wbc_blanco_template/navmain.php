f880e51eb03f621f281d603507dc8bcd7314032c
Claudia Oerter <coweb01@users.noreply.github.com>
2024-03-28 12:26:10 +0100

    svg icon

--- html/layouts/wbc_blanco_template/navmain.php
+++ html/layouts/wbc_blanco_template/navmain.php
@@ -33,12 +33,13 @@
 <?php endif;  ?>
 <div class="navbar-button">
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#OffcanvasMenu<?php echo $offcanvas_pos; ?>" aria-controls="OffcanvasMenu<?php echo $offcanvas_pos;?>" aria-label="<?php echo Text::_('TPL_WBC_MENU'); ?>">
-    <svg xmlns="http://www.w3.org/2000/svg" class="icon" aria-hidden="true" focusable="false" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-        <path 
-            fill="currentColor"
-            d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"
-            />
-    </svg>
+        <svg class="svg-toggler-icon" aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg">
+            <g fill="none" stroke="currentColor">
+                <path class="icon-line1" stroke-width="3" d="M5 5 l50 0" />
+                <path class="icon-line2" stroke-width="3" d="M5 15 l50 0" />
+                <path class="icon-line3" stroke-width="3" d="M5 25 l50 0" />
+            </g>
+        </svg> 
     </button>
     <span class="wbc__offcanvas-toggler-button-txt" aria-hidden="true"><?php echo Text::_('TPL_WBC_MENU'); ?></span>
 </div>
