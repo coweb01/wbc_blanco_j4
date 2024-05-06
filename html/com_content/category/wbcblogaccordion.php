148b39a6e1d3797c20efa33fd86c5e0488ec8883
Claudia Oerter <coweb01@users.noreply.github.com>
2024-05-06 10:53:28 +0200

    Pagination zugefügt

--- html/com_content/category/wbcblogaccordion.php
+++ html/com_content/category/wbcblogaccordion.php
@@ -199,4 +199,17 @@ $htag    = $this->params->get('show_page_heading') ? 'h2' : 'h1';
         <?php endforeach; ?>
         </div>
     <?php endif; ?>
+   
+    <?php if (($this->params->def('show_pagination', 1) == 1 || ($this->params->get('show_pagination') == 2)) && ($this->pagination->pagesTotal > 1)) : ?>
+        <div class="com-content-category-blog__navigation w-100">
+            <div class="com-content-category-blog__pagination">
+                <?php echo $this->pagination->getPagesLinks(); ?>
+            </div>
+            <?php if ($this->params->def('show_pagination_results', 1)) : ?>
+                <p class="com-content-category-blog__counter counter small text-center">
+                    <?php echo $this->pagination->getPagesCounter(); ?>
+                </p>
+            <?php endif; ?>
+        </div>
+    <?php endif; ?>
 </div>
