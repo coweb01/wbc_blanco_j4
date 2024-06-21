86f1defe6d4a9141440c8ad0dd3f56e9b0786783
Claudia Oerter <coweb01@users.noreply.github.com>
2024-06-21 12:47:25 +0200

    Update component.php
    add facicons

--- component.php
+++ component.php
@@ -1,5 +1,4 @@
 <?php
-
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
@@ -13,6 +12,7 @@ $wa  = $this->getWebAssetManager();
 $doc = $app->getDocument();
 $templateparams = $app->getTemplate(true)->params; // Templateparameter
 $fontawesome = $templateparams->get('fontawesome');
+$mediapath   = 'media/templates/site/'.$this->template.'/';
 
 // generator tag
 $this->setGenerator(null);
@@ -36,8 +36,19 @@ $wa->usePreset('template.wbc')
 	->useStyle('template.user')
 	->useScript('template.user');
 
-?>
+// Favicons https://realfavicongenerator.net/
+$this->addHeadLink(HTMLHelper::_('image', 'favicons/apple-touch-icon.png', '', [], true, 1), 'apple-touch-icon', 'rel', ['sizes' => '180x180']);
+$this->addHeadLink(HTMLHelper::_('image', 'favicons/favicon-32x32.png', '', [], true, 1), 'icon', 'rel', ['sizes' => '32x32', 'type' => 'image/png']);
+$this->addHeadLink(HTMLHelper::_('image', 'favicons/favicon-16x16.png', '', [], true, 1), 'icon', 'rel', ['sizes' => '16x16', 'type' => 'image/png']);
+$this->addHeadLink(HTMLHelper::_('image', 'favicons/safari-pinned-tab.svg', '', [], true, 1), 'mask-icon', 'rel', ['color' => '#41599a']);
+$this->addHeadLink(HTMLHelper::_('image', 'favicons/favicon.ico', '', [], true, 1), 'shortcut icon', 'rel', ['type' => 'image/vnd.microsoft.icon']);
+$this->addHeadLink($mediapath . 'images/favicons/site.webmanifest', 'manifest', 'rel', []);
+$this->setMetaData('msapplication-config', $mediapath . 'images/favicons/browserconfig.xml');
+$this->setMetaData('theme-color', '#ffffff');
 
+// Defer font awesome
+$wa->getAsset('style', 'fontawesome')->setAttribute('rel', 'lazy-stylesheet');
+?>
 <!DOCTYPE html>
 <html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 <head>
