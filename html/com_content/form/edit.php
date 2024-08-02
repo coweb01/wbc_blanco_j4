121335de9d09dacf656b7d9b091ea0fcdb016591
Claudia Oerter <coweb01@users.noreply.github.com>
2024-08-02 12:47:02 +0200

    Update edit.php

--- html/com_content/form/edit.php
+++ html/com_content/form/edit.php
@@ -43,7 +43,7 @@ $app          = Factory::getApplication();
 // Frontend editing Buerger
 $template     = $app->getTemplate();
 $mediapath    = 'media/templates/site/'. $template .'/';
-$wa->registerAndUseStyle('frontendedit', $mediapath . 'css/frontend_edit.css');
+$wa->registerAndUseStyle('frontendedit', $mediapath . 'css/frontend_edit.css',[], [], ['core','template.wbc']);
 
 $usergroups     = $this->user->groups; 
 $groupIds       = $this->user->getAuthorisedGroups();
