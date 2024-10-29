94d127f4557f4c3ff15aaa64d059a2c9c1b5fca5
Viviana Menzel <vivianamenzel@dr-menzel-it.de>
2024-10-29 11:56:06 +0100

    Entfernen falsch platziertes </a>

--- html/com_content/category/wbcbs5accordion_children.php
+++ html/com_content/category/wbcbs5accordion_children.php
@@ -27,17 +27,17 @@ if ($this->maxLevel != 0 && count($this->children[$this->category->id]) > 0) : ?
                 <?php $HtmlImage =  HTMLHelper::_('image', $child->getParams()->get('image'), '' , ['class' => 'card-img card-img-top inline-svg']); ?>
         <?php } else { ?>
                 <?php $HtmlImage =  HTMLHelper::_('image', $child->getParams()->get('image'), $child->getParams()->get('image_alt'), ['class' => 'card-img card-img-top inline-svg']); ?>
-        <?php } ?>        
+        <?php } ?>
         <?php // Check whether category access level allows access to subcategories. ?>
         <?php if (in_array($child->access, $groups)) : ?>
             <?php if ($this->params->get('show_empty_categories') || $child->numitems || count($child->getChildren())) : ?>
             <?php if( $this->params->get('num_columns') > 1 ) { ?>
                 <div class="col align-items-center <?php echo $cssCol;?>">
-            <?php } ?>    
-                <a href="<?php echo Route::_(RouteHelper::getCategoryRoute($child->id, $child->language)); ?>" class="wbc__card-link">     
+            <?php } ?>
+                <a href="<?php echo Route::_(RouteHelper::getCategoryRoute($child->id, $child->language)); ?>" class="wbc__card-link">
 
-                    <div class="wbc__com-content-category-blog__child card "> 
-                        <div class="wbc__card-body card-body"> 
+                    <div class="wbc__com-content-category-blog__child card ">
+                        <div class="wbc__card-body card-body">
                                 <?php if ( $child->getParams()->get('image')) : ?>
                                     <div class="wbc__bg-secondary wbc__border_icon wbc__card_icon wbc__grow">
                                     <?php echo $HtmlImage; ?>
@@ -55,7 +55,7 @@ if ($this->maxLevel != 0 && count($this->children[$this->category->id]) > 0) : ?
                                 </h3>
                                 <?php else : ?>
                                 <h3 class="wbc__card-title card-title text-center mt-3 h4">
-                                    <?php echo $this->escape($child->title); ?></a>
+                                    <?php echo $this->escape($child->title); ?>
                                     <?php if ($this->params->get('show_cat_num_articles', 1)) : ?>
                                         <span class="badge bg-info">
                                             <?php echo Text::_('COM_CONTENT_NUM_ITEMS'); ?>&nbsp;
@@ -64,7 +64,7 @@ if ($this->maxLevel != 0 && count($this->children[$this->category->id]) > 0) : ?
                                     <?php endif; ?>
                                 </h3>
                                 <?php endif; ?>
-                                
+
                                 <?php if ($this->params->get('show_subcat_desc') == 1) : ?>
                                     <?php if ($child->description) : ?>
                                     <div class="com-content-category-blog__description category-desc wbc__card-text card-text text-center">
@@ -74,10 +74,10 @@ if ($this->maxLevel != 0 && count($this->children[$this->category->id]) > 0) : ?
                                 <?php endif; ?>
                         </div>
                     </div>
-                </a>   
+                </a>
                 <?php if( $this->params->get('num_columns') > 1 ) { ?>
                 </div>
-            <?php } ?> 
+            <?php } ?>
             <?php endif; ?>
         <?php endif; ?>
     <?php endforeach; ?>
