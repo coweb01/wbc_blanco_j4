0be6ff610b0f51d64a1273fa31dbabe8004ade28
Claudia Oerter <coweb01@users.noreply.github.com>
2024-10-31 12:55:48 +0100

    Update edit.php

--- html/com_content/form/edit.php
+++ html/com_content/form/edit.php
@@ -26,7 +26,7 @@ use Joomla\CMS\Table\Table;
 use Joomla\CMS\Factory;
 
 /** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
-$wa = $this->document->getWebAssetManager();
+$wa = $this->getDocument()->getWebAssetManager();
 $wa->useScript('keepalive')
     ->useScript('form.validate')
     ->useScript('com_content.form-edit');
